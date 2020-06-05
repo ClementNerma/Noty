1d01d4bd08a3a94fcc2b12e9a8eb442dfd8403b3
Clément Nerma <clement.nerma@gmail.com>
2020-06-05 18:00:26 +0200

    Comment the loader

--- src/view/loader.ts
+++ src/view/loader.ts
@@ -4,6 +4,10 @@ import * as zlib from 'zlib'
 import { remote } from 'electron'
 import { MaybeUninit } from 'typescript-core'
 
+/**
+ * Raise an error during loading
+ * @param errorMessage
+ */
 const loadingError = (errorMessage: string) => {
   remote.dialog.showMessageBoxSync({
     type: 'error',
@@ -16,18 +20,22 @@ const loadingError = (errorMessage: string) => {
   throw new Error(errorMessage)
 }
 
+// Ensure we can require() modules
 if (typeof require !== 'function') {
   loadingError('Cannot load main script because "require" function is not available in this context')
 }
 
+// Get the current timestamp to measure startup time
 const started = Date.now()
 
+// Ensure the Ace.js bundle exists
 const aceBundlePath = path.join(__dirname, 'ace.js', 'ace-bundle.gz')
 
 if (!fs.existsSync(aceBundlePath)) {
   loadingError(`Ace.js bundle was not found at path "${aceBundlePath}"`)
 }
 
+// Read the Ace.js bundle
 let bundleContent = new MaybeUninit<Buffer>()
 try {
   bundleContent.init(fs.readFileSync(aceBundlePath))
@@ -35,6 +43,7 @@ try {
   loadingError(`Failed to read Ace.js bundle at path "${aceBundlePath}":\n${e.message}`)
 }
 
+// Decompress the Ace.js bundle
 let decompressedBundle = new MaybeUninit<string>()
 try {
   decompressedBundle.init(zlib.inflateSync(bundleContent.unwrap()).toString('utf8'))
@@ -44,16 +53,19 @@ try {
 
 console.debug(`Decompressed Ace.js bundle in ${Date.now() - started} milliseconds.`)
 
+// Run Ace.js initialization and declaration scripts
 try {
   window.eval(decompressedBundle.unwrap())
 } catch (e) {
   loadingError(`A JavaScript error occurred while evaluating decompressed Ace.js bundle:\n${e.stack}`)
 }
 
+// Ensure the application's main file exists
 const mainPath = path.join(__dirname, '..', 'front', 'index.js')
 
 if (!fs.existsSync(mainPath)) {
   loadingError(`Application's main file was not found at path "${mainPath}"`)
 }
 
+// Load it (raw import is used to trigger side effects without actually importing anything)
 import '../front/index'
